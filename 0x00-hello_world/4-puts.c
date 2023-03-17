272ac520006dd6b8d2c9d15d2626cfbbae4b32db
elvissio <onunwaelvis@gmail.com>
2023-03-17 14:34:11 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
 * main - Entry point
 *
 * Return: Always 0 (Success)
@@ -7,5 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
