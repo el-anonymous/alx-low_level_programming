0e4018c62baaf5cdfe1cc95e61c80296e44eff9f
elvissio <onunwaelvis@gmail.com>
2023-03-21 20:17:34 +0000

    Project 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * main - check the code
+ *  * print_alphabet - Prints the alphabet in lowercase.
  *   *
- *    * Return: Always 0.
+ *    * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
