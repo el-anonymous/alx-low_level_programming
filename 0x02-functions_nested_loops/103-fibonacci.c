7f8edc5d8646adfb94f4e4b144c01afe8abfbe5e
Elvis Termux <onunwaelvis@gmail.com>
2023-03-22 02:22:37 +0100

    Error Fixed

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	int a = 1, b = 2, c, sum = 2;
 
 	while (b <= 4000000)
 	{
-        	c = a + b;
-        if (c % 2 == 0)
-	{
-		sum += c;
-        }
+		c = a + b;
+        	if (c % 2 == 0)
+		{
+            		sum += c;
+        	}
         	a = b;
         	b = c;
-    	}
+	}
 
-    	printf("%d\n", sum);
-    	return 0;
+	printf("%d\n", sum);
+	return (0);
 }
+
