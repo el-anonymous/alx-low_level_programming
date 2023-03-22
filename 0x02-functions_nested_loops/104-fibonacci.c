e72e93c86e3199d0018a10a1f9261800a4be2a69
Elvis Termux <onunwaelvis@gmail.com>
2023-03-22 02:05:49 +0100

    Error Fixed

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1, b = 2, c;
+    printf("%d, %d", a, b);
+
+    for (int i = 2; i < 98; i++) {
+        c = a + b;
+        printf(", %d", c);
+        a = b;
+        b = c;
+    }
+    printf("\n");
+    return 0;
+}
+
