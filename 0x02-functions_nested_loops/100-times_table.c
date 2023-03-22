f74408f302cb3d1996779ebd7da4a741a26fcdab
Elvis Termux <onunwaelvis@gmail.com>
2023-03-22 01:45:34 +0100

    Error Fixed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,17 +2,25 @@
 
 void print_times_table(int n) 
 {
-    if (n < 0 || n > 15) 
-    {
-        return;
-    }
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= n; j++)
+	if (n < 0 || n > 15) 
 	{
-            _putchar("%d ", i * j);
+		return;
+	}
+	for (int i = 0; i <= n; i++) 
+	{
+		for (int j = 0; j <= n; j++) {
+            	int product = i * j;
+            	if (product < 10) 
+	    	{
+		    	_putchar(product + '0');
+            	} else 
+	    	{
+		    	_putchar((product / 10) + '0');
+		    	_putchar((product % 10) + '0');
+            	}
+            	_putchar(' ');
         }
-        _putchar("\n");
+        _putchar('\n');
     }
 }
 
