3f8c7e82fc93b401fae725c694abdc95036fd15f
el-anonymous <onunwaelvis@gmail.com>
2023-03-24 11:41:07 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
-#include "holberton.h"
-
 /**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
- */
+* _isupper - checks if parameter is an uppercase character.
+* @c: input character.
+* Return: 1 if is an uppercase character, 0 in other case.
+*/
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
