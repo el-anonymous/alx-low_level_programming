9e3079b675479e1b95e429543535e2147c76e7ba
Elvis Onunwa <69672301+el-anonymous@users.noreply.github.com>
2023-04-18 01:03:58 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    printf("This program was compiled from the file: %s\n", __FILE__);
-    return 0;
+/**
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
